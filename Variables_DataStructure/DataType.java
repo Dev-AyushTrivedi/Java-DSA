b910e920e21600ff8e02aabe5dc30d4f734609a4
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-06 12:04:57 +0530

    Add files via upload

---
+++ Variables_DataStructure/DataType.java
@@ -0,0 +1,29 @@
+public class DataType {
+    public static void main(String[] args) {
+        // Byte DataType 
+        byte b = 8;
+        System.out.println(b);
+        // Chararcter DataType
+        char Ch = 'a';
+        System.out.println(Ch);
+        // Boolean Datatype 
+        boolean var = true;
+        System.out.println(var);
+        // Integer DataType 
+        int A = 45;
+        System.out.println(A);
+        // Float DataType
+        float Price = 5;
+        System.out.println(Price);
+        /*Short DataType */
+        short val = 1000;
+        System.out.println(val);
+        // Long Datatype 
+        long Value = 10000000;
+        System.out.println(Value);
+        // Double Datatype 
+        double number = 99.99999989;
+        System.out.println(number);
+
+    }    
+}

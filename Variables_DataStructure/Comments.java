46fa1473c4b46dc1720f2106f683cee0c831abee
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-07 17:01:15 +0530

    Add files via upload

---
+++ Variables_DataStructure/Comments.java
@@ -0,0 +1,11 @@
+public class Comments {
+    public static void main(String args[]){
+        // This is a single line comments in Java
+        /*
+         * this is 
+         * multiple line comments 
+         * in
+         * Java
+         */
+    }
+}

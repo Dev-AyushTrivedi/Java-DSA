a88f432d238f674491d9df8f86f8ee3c369e16d4
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-12 13:43:29 +0530

    Add files via upload

---
+++ Conditional_Statement's/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 22;
+        if (age >= 18); {
+            System.out.println("You are an Adult:  Vote, Drive");
+        }
+
+        int age1 = 15;
+        if ( age1 > 13 && age1 < 18){
+            System.out.println("You are a Teenager");
+        }
+        else {
+            System.out.println("You are a Child");
+        }
+    }
+}

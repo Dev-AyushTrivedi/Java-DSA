c884bb14c93a0548ef1ed99e6ce69f8d76771e99
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-14 11:45:28 +0530

    Delete Conditional_Statement's directory

--- Conditional_Statement's/TernaryOperator.java
+++
@@ -1,9 +0,0 @@
-public class TernaryOperator {
-    public static void main(String[] args) {
-        int Number = 4;
-
-        String type = ((Number%2) == 0) ? "Even" : "Odd";
-
-        System.out.println(type);
-    }
-}

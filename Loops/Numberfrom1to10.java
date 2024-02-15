c0ee1afe2a92a5c050fd71fb198e7e851f518525
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-15 16:48:40 +0530

    Add files via upload

---
+++ Loops/Numberfrom1to10.java
@@ -0,0 +1,10 @@
+public class Numberfrom1to10 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter <= 10){
+            System.out.print(counter+" "); //for printing in same line with space use only print along with " "
+            System.out.println(counter);  //for printing in multiple line with space use only println
+            counter++;
+        }
+    }
+}

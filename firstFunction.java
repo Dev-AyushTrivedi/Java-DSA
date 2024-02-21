4f35402c597420c46c8cb11b716774dc1945ecf4
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-21 13:42:35 +0530

    Delete firstFunction.java

--- firstFunction.java
+++
@@ -1,8 +0,0 @@
-public class firstFunction{
-    public static void printHelloWolrd(){
-        System.out.println("hello world!");
-    }
-    public static void main(String[] args) {
-        printHelloWolrd();
-    }
-}

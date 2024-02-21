68572148292c364e02743caab374e0207a1c2857
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-21 13:41:36 +0530

    first function program

---
+++ firstParameter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class firstParameter {
+    public static void calculateSum(int num1, int num2){
+        int Sum = num1 + num2;
+        System.out.println("Sum is = " + Sum);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        calculateSum(a, b);
+    }
+}

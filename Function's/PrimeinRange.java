b0735df8c869590d36dbe1fbbac800ed695622fb
Ayush Trivedi <150885930+Dev-AyushTrivedi@users.noreply.github.com>
2024-02-26 21:16:00 +0530

    prime number question

---
+++ Function's/PrimeinRange.java
@@ -0,0 +1,23 @@
+public class PrimeinRange {
+    public static boolean isPrime(int n){
+        if(n == 2){
+            return true;
+        }
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;            
+            }
+        }
+        return true;
+    }
+    public static void PrimeInRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){ //if true
+                System.out.println(i+ " ");
+            }
+        }
+    }
+    public static void main(String args[]){
+        PrimeInRange(20);
+    }
+}

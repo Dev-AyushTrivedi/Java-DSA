7fcccae0dd45feca0b3a82753b58901adf39f690
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-03-02 16:29:50 +0530

    new Code added

--- Advance_Pattern's/Hollow_Rectangle_pattern.java
+++ Advance_Pattern's/Hollow_Rectangle_pattern.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Hollow_Rectangle_pattern {
+    public static void rectangle(int tolRow, int tolCol){
+
+        for(int i=1; i<=tolRow; i++){
+            for(int j=1; j<=tolCol; j++){
+                if(i == 1 || i == tolRow || j == 1 || j == tolCol){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        System.out.print("Enter no of Columns: ");
+        Scanner sc = new Scanner(System.in);
+        int tolCol=sc.nextInt();
+        System.out.println();
+
+        rectangle(tolCol, tolCol);
     }
 }
--- Array's/FirstArray.java
+++ Array's/FirstArray.java
@@ -1,4 +1,10 @@
 public class FirstArray{
+     // call by reference    
+    public static void update(int marks2[]){
+        for(int i = 0; i < marks2.length; i++){
+            marks2[i] = marks2[i] + 1;
+        }
+    }
     public static void main(String[] args) {
         int marks[] = new int[50];
 
@@ -7,5 +13,15 @@ public class FirstArray{
         int moreNumbers[] = {11,22,33,44,55,66,77,88,99,00};
 
         String fruits[] = {"Apple", "Mengo", "Orange"};
+        
+
+
+        int marks2[] = {97,89,99}; // call by reference
+        update(marks2);
+
+        for(int i = 0; i < marks2.length; i++){
+            System.out.print(marks2[i]+" ");
+        }
+        System.out.println();
     }
 }

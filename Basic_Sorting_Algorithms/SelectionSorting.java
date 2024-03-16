5a561faf1db3c15b334469d65cbf6c05fd9506a8
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-03-16 10:49:32 +0530

    Selection Sort

---
+++ Basic_Sorting_Algorithms/SelectionSorting.java
@@ -0,0 +1,29 @@
+public class SelectionSorting{
+    
+    public static void SelectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printAlgo(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int arr[] = {5, 4, 1, 3, 2, 7, 9, 10, 8, 6};
+        SelectionSort(arr);
+        printAlgo(arr);
+    }
+}
+
+// time complexity = O(n^2)

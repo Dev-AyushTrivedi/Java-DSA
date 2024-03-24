676eb5aa4ec691edf6cc7381254b4ae09c42f515
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-03-24 21:10:58 +0530

    loops practice

---
+++ Practice4/practice2.java
@@ -0,0 +1,10 @@
+public class practice2 {
+    public static void main(String[] args) {
+        int n=10;
+        int m=1;
+        while (m <= n) {
+            System.out.println(m+" ");
+            m++;
+        }
+    }   
+}

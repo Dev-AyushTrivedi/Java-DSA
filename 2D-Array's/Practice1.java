1f3b0d80d85d32c85eecc4b38ebccc9fd978acc6
Ayush Trivedi <ayushtrivedi890@gmail.com>
2024-03-26 12:21:12 +0530

    Strings

--- 2D-Array's/Practice1.java
+++ 2D-Array's/Practice1.java
@@ -1,5 +1,15 @@
 public class Practice1{
     public static void main(String[] args) {
-        
+        int [][] arr = {{4,7,8},{8,8,6}};
+
+        int countOf7 = 0;
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[0].length; j++) {
+                if(arr[0][j]==7){
+                    countOf7++;
+                }
+            }
+        }
+        System.out.println("count of 7 " + countOf7);
     }
 }
---
+++ Strings/FirstString.java
@@ -0,0 +1,7 @@
+public class FirstString {
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', };
+        String atr = "abcd";
+        String str2 = new String("xyz");
+    }
+}
